79f7f75427bc28effa11bd1e6246f94e875f3768
sandy.fang <751541230@qq.com>
2018-08-09 16:22:00 +0800

    fix a bug

--- apps/transfer/views.py
+++ apps/transfer/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from .utils import switch_get, switch_post
 
 from utils.http import APIResponse
+from utils.response import res_code
 
 
 class RecordList(APIView):
@@ -9,12 +10,18 @@ class RecordList(APIView):
         module = request.query_params.get('module')
         if module:
             module_prefix = module.split('_')[0]
-        response = switch_get(module_prefix, request)
+            response = switch_get(module_prefix, request)
+        else:
+            data = 'Need a parameter of module.'
+            return APIResponse(rescode=res_code['error'], data=data)
         return response
 
     def post(self, request):
-        module = request.data['module']
-        if module:
-            module_prefix = module.split('_')[0]
+        try:
+            module = request.data['module']
+        except Exception as e:
+            data = 'Need input a parameter of module.'
+            return APIResponse(rescode=res_code['error'], data=data)
+        module_prefix = module.split('_')[0]
         data, status = switch_post(module_prefix, request.data)
         return APIResponse(rescode=status, data=data)
